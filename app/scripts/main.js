4dc055ff281ed5a831c3f93f200690a74ad9ac1d
Andy Georgia <andygeorgia@hotmail.com>
2014-06-03 15:20:00 -0400

    new stuff

---
+++ app/scripts/main.js
@@ -0,0 +1,25 @@
+List = Backbone.Model.extend({
+
+  initialize: function() {
+    this.on('change', function() {
+    })
+  },
+  defaults: {
+
+  },
+  idAttribute: '_id'
+});
+
+ListCollection = Backbone.Collection.extend( {
+
+  model: List,
+
+  url: 'http://tiny-pizza-server.herokapp.com/collections/andys-stuff',
+})
+
+
+ListView = Backbone.View.extend( {
+
+  template: _.template($('.list-item').text()),
+  editTemplate: _.template($('.list-edit-item')),
+})
